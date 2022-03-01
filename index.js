5521a0b871dc025dca90e08ceff0109aea411c6f
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-03-01 16:21:25 +0900

    code format

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ client.once("ready", async () => {
   );
 });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const command = commands[interaction.commandName];
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
 client.on("guildCreate", (guild) => {
   const Embed = new MessageEmbed(guild)
     .setColor([88, 101, 242])
@@ -52,19 +68,4 @@ client.on("guildCreate", (guild) => {
     .send({ embeds: [Embed] });
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
-  const command = commands[interaction.commandName];
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
 client.login(process.env.BOT_TOKEN);
